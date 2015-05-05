8977a196b28f028fdd89f23651623cc8e4db2a06
sderhy <serge@derhy.com>
2015-05-06 00:18:45 +0200

    Translation
    Variables name were translated to more understandable english.
    One more example file, and some explanation in the README.md

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# camembert
-Camembert is a very simple "pie-chart" Jquery plugin.
-It integrates a simple SVG pie chart, without writing a simple line of svg code.
+# Camembert
+Camembert is a very simple "pie-chart" jQuery plugin.
+It integrates a simple SVG pie chart, without writing a single  line of svg code.
+
+## Camembert in brief
+
+- The target element must exists in your html file. At least you must provide two data arrays of the same size, one is an array of string labels, the second is an array of float values corresponding to the percentage of each corresponding label value. 
+
+## Usage
+
+- Include jQuery and Camembert
+- Call Camembert
+
+```html
+<script src="jquery.js"></script>
+<script src="jquery.camembert.js"></script>
+<script>
+	$(document).ready(function(){
+
+		// Some delicious french cheese : 
+		var labels = ["Brie", "Camembert", "Pont-Lévêque", "Munster", "Vacherin"] ; 
+
+		// Somes values:
+		var values = [20.10 , 42.30 , 19.60 , 14.70 , 3.30 ] ; 
+
+		$("#camembert").camembert({ "labels": labels , "values": values }) ; 
+
+	}); 
+</script>
+```
+## 
 
--- plugin/jquery.camembert.js
+++ plugin/jquery.camembert.js
@@ -1,50 +1,46 @@
-(function($)
-{ 
+(function($){ 
 	$.fn.camembert =function(opdata){  
-		var angleDepart =0 ; 
-		var angleFin=0 ;   
-        //On définit nos paramètres par défaut
-	    var defauts=
-	    {
-			angleDepart: 0,		
-			angleFin: 0,		
-			rayon: 100,		
-			tabLabel: ["Developpement","Graphisme","Recette", "Administratif"],
-			tabDonnees:  [35,45,15,5] ,
-			tabCouleur: [] 
-	    };  
-	           
-	    //On fusionne nos deux objets ! =D
-	   	var obj = $.extend(defauts, opdata );
+
+		var startAngle =0 ; 
+		var endAngle=0 ;   
+
+		//Defaults parameters:
+		var defaults=
+		{
+				startAngle: 0,		
+				endAngle: 0,		
+				radius: 100,		
+				labels: ["Developpement","Graphisme","Recette", "Administratif"],
+				values:  [35,45,15,5] ,
+				colors: [] 
+		};  
+			
+		//Melting the two objects:
+		var obj = $.extend(defaults, opdata );
 
 
-	   	function init(obj){
-	   		if(obj['tabCouleur'].length == 0 ){
-	   				$.each( obj.tabDonnees, function(index,value){
-	   					obj.tabCouleur.push(getRandomColor());
-	   				});
-	   		}
-	   	}
+		function init(obj){
+			if(obj['colors'].length == 0 ){
+					$.each( obj.values, function(index,value){
+						obj.colors.push(getRandomColor());
+					});
+			}
+		}
 
 		function drawGraphCam(obj, div){
 
 			init(obj) ; 
 
-		 	div.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width=500 height=600></svg>';
-			 // pour chaque entree des tableaux on dessine les arc , carres et textes
-			 for (i=0; i<obj['tabDonnees'].length;i++){
+			div.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width=500 height=600></svg>';
+
+			for (i=0; i<obj['values'].length;i++){
 				addElements(i);
-				angleDepart=angleFin;
+				startAngle=endAngle;
 
 			}
-			//return this;
-		 } 
+		} 
 
-		
-
-
-
-	    function getRandomColor() {
+		function getRandomColor() {
 			var letters = '0123456789ABCDEF'.split('');
 			var color = '#';
 			for (var i = 0; i < 6; i++ ) {
@@ -53,72 +49,72 @@
 			return color;
 		}
 
-		function degres2radians(centreX, centreY, rayon, degres) {
-			 var radians = (degres-90) * Math.PI / 180.0;
-			 return {
-				 x: centreX + (rayon * Math.cos(radians)),
-				 y: centreY + (rayon * Math.sin(radians))
-			 };
+		function degres2radians(centreX, centreY, radius, degres) {
+			var radians = (degres-90) * Math.PI / 180.0;
+			return {
+				x: centreX + (radius * Math.cos(radians)),
+				y: centreY + (radius * Math.sin(radians))
+			};
 		}
-		function monArc(x, y, rayon, angleDepart, angleFin){
-			 var depart = degres2radians(x, y, rayon, angleFin);
-			 var fin = degres2radians(x, y, rayon, angleDepart);
-			 var arc180 = angleFin - angleDepart <= 180 ? "0" : "1";
-			 var d = [
-			 "M", depart.x, depart.y,
-			 "A", rayon, rayon, 0, arc180, 0, fin.x, fin.y,
-			 "L", x,y,
-			 "L", depart.x, depart.y
-			 ].join(" ");
-			 
+
+		function arcPath(x, y, radius, startAngle, endAngle){
+			var depart = degres2radians(x, y, radius, endAngle);
+			var fin = degres2radians(x, y, radius, startAngle);
+			var arc180 = endAngle - startAngle <= 180 ? "0" : "1";
+			var d = [
+			"M", depart.x, depart.y,
+			"A", radius, radius, 0, arc180, 0, fin.x, fin.y,
+			"L", x,y,
+			"L", depart.x, depart.y
+			].join(" ");
+			
 			return d;
 		}
-			 
+				
 		function addElements(i){
-			 var svg =  $('svg')[0]; //Get le svg
-			 angleDepart=angleDepart;
-			 angleFin=(angleDepart+obj['tabDonnees'][i]);
+			var svg =  $('svg')[0]; //Get le svg
+			startAngle=startAngle;
+			endAngle=(startAngle+obj['values'][i]);
 
-			 // dessin des arcs de cercle
-			 var path = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
-			 path.setAttribute('fill',obj['tabCouleur'][i]);
-			 path.setAttribute('d',monArc(120 , 120, obj['rayon'], angleDepart*3.6, angleFin*3.6));
-			 path.setAttribute('id','arc'+i);
-			 path.setAttribute('p',i);
-			 path.setAttribute('class',"arc");
-			 
-			 // dessin des carres de legende
-			 var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect'); //Create a path in SVG's namespace
-			 rect.setAttribute('width','16');
-			 rect.setAttribute('height','16');
-			 rect.setAttribute('x',260);
-			 rect.setAttribute('y',30*i+10);
-			 rect.setAttribute('fill',obj['tabCouleur'][i]);
-			 rect.setAttribute('id','rect'+i);
-			 
-			 // dessin des textes de legende
-			 var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-			 text.setAttribute('x', 280);
-			 text.setAttribute('y', 30*i+10+12);
-			 text.setAttribute('fill', '#999');
-			 text.setAttribute('font-size','12');
-			 text.setAttribute('font-family','sans-serif');
-			 text.setAttribute('font-weight','normal');
-			 text.setAttribute('id','text'+i);
-			 text.textContent = obj['tabLabel'][i]+' ('+obj['tabDonnees'][i]+'%)';
-			 
-			 // ajout des elements au svg
-			 svg.appendChild(path);
-			 svg.appendChild(rect);
-			 svg.appendChild(text);
+			// Draw circle bow:
+			var path = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+			path.setAttribute('fill', obj['colors'][i]);
+			path.setAttribute('d', arcPath(120 , 120, obj['radius'], startAngle*3.6, endAngle*3.6));
+			path.setAttribute('id', 'arc'+i);
+			path.setAttribute('p', i);
+			path.setAttribute('class', "arc");
+			
+			// Draws the legend rects
+			var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect'); //Create a path in SVG's namespace
+			rect.setAttribute('width','16');
+			rect.setAttribute('height','16');
+			rect.setAttribute('x',260);
+			rect.setAttribute('y',30*i+10);
+			rect.setAttribute('fill',obj['colors'][i]);
+			rect.setAttribute('id','rect'+i);
+			
+			// Draws the text labels:
+			var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+			text.setAttribute('x', 280);
+			text.setAttribute('y', 30*i+10+12);
+			text.setAttribute('fill', '#999');
+			text.setAttribute('font-size','12');
+			text.setAttribute('font-family','sans-serif');
+			text.setAttribute('font-weight','normal');
+			text.setAttribute('id','text'+i);
+			text.textContent = obj['labels'][i]+' ('+obj['values'][i]+'%)';
+			
+			// Append to svg 
+			svg.appendChild(path);
+			svg.appendChild(rect);
+			svg.appendChild(text);
 		}
 
 
 
-	     return this.each(function( i,o ){
-		    	//console.log("plugin camembert i : ", i , "objet: ", o); 
-	      	    drawGraphCam(obj, o) ;  // doit renvoyer this en fin de traitement ; 
-	     });
+		return this.each(function( i,o ){
+			drawGraphCam(obj, o) ;  
+		});
 			
 
 	}
